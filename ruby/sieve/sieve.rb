fe5a9ecb933c9e6377561be22d101f2baadc4137
Mike Calhoun <mcalhoun@centresource.com>
2013-08-23 08:22:49 -0500

    finally adding this to github, though not sure of why

---
+++ ruby/sieve/sieve.rb
@@ -0,0 +1,17 @@
+class Sieve
+
+  def initialize(highest_value)
+    @primes = []
+    @max = highest_value
+    find_prime_numbers(@max)
+    #start at 2 and go up until limit
+    #if next number mod current number == 0 igore, else add to primes?
+  end
+
+
+  def find_primes(upper_limit)
+
+
+  end
+
+end
